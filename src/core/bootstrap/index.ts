637e0c328ea30c97e899af8f3b14f024bcbb9691
Serchinastico <github@serchinastico.com>
2024-09-13 18:05:20 +0200

    feat: add splash screen

--- src/core/bootstrap/index.ts
+++ src/core/bootstrap/index.ts
@@ -1,6 +1,14 @@
 import "./translations";
 
+/**
+ * We need 👆 those imports to be before anything else
+ * that's why we ignore perfectionist's rules
+ */
+// eslint-disable-next-line perfectionist/sort-imports
+import * as SplashScreen from "expo-splash-screen";
+
 export const bootstrap = () => {
+  SplashScreen.preventAutoHideAsync();
   /**
    * Put here everything that must be executed before the app
    * loads
--- src/core/providers/AllProviders.tsx
+++ src/core/providers/AllProviders.tsx
@@ -3,12 +3,16 @@ import { I18nProvider } from "@lingui/react";
 import { TelarContextProvider } from "@madeja-studio/telar";
 import { PropsWithChildren } from "react";
 
+import PreloadScreen from "../bootstrap/PreloadScreen";
+
 interface Props extends PropsWithChildren {}
 
 const AllProviders = ({ children }: Props) => {
   return (
     <TelarContextProvider>
-      <I18nProvider i18n={i18n}>{children}</I18nProvider>
+      <PreloadScreen>
+        <I18nProvider i18n={i18n}>{children}</I18nProvider>
+      </PreloadScreen>
     </TelarContextProvider>
   );
 };
