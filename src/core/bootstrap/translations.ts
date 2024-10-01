bd3accd2bb047d56dfa9e028e75f782088549ce3
Serchinastico <github@serchinastico.com>
2024-10-01 12:04:03 +0200

    fix: make sure the app uses one of the provided locales and not an unknown one

--- src/core/bootstrap/translations.ts
+++ src/core/bootstrap/translations.ts
@@ -3,7 +3,16 @@ import { messages as esMessages } from "@app/locales/es/messages";
 import { i18n } from "@lingui/core";
 import * as Localization from "expo-localization";
 
-const locales = Localization.getLocales().map((locale) => locale.languageCode);
-i18n.load("en", enMessages);
-i18n.load("es", esMessages);
-i18n.activate(locales[0] ?? "en");
+const LOCALES = { en: enMessages, es: esMessages };
+const ALLOWED_LOCALES = Object.keys(LOCALES);
+const DEFAULT_LOCALE: keyof typeof LOCALES = "en";
+
+Object.entries(LOCALES).forEach(([locale, messages]) => {
+  i18n.load(locale, messages);
+});
+
+const userLocale = Localization.getLocales()
+  .map((locale) => locale.languageCode)
+  .find((code) => code && ALLOWED_LOCALES.includes(code));
+
+i18n.activate(userLocale ?? DEFAULT_LOCALE);
